fc2361d5b76a3371e4d0eef8d4f40c16757ca9b6
julianmedwards <julian.michael.edwards@gmail.com>
2024-07-12 20:18:22 -0500

    Change to C#, dotnet console app gen

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿namespace FITR;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Fighter Input & Timing Recorder
-
-A game-agnostic tool for recording, examining, practicing and sharing fighting game combos.
